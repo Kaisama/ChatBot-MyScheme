31f25de3c2ac46ccfec5d143b78a8817df191ffa
Akshat Bisht <bishtakshat64@gmail.com>
2024-08-14 11:20:13 +0530

    message parser completed

--- src/chatBot/MessageParser.jsx
+++ src/chatBot/MessageParser.jsx
@@ -1,20 +1,40 @@
-import { cloneElement } from "react";
-import { Children } from "react";
+import { cloneElement, Children } from "react";
 
 const MessageParser = ({ children, actions, state }) => {
-  const { askingForDistrict, askingForAge } = state || {};
-console.log("askingForDistrict",askingForDistrict);
-
   const parse = (message) => {
     const lowerCaseMsg = message.toLowerCase();
-    if (children.props.state.askingForDistrict) {
+
+    if (children.props.state.askingForName) {
+      actions.handleNameCheck(lowerCaseMsg);
+    } else if (children.props.state.askingForEmail) {
+      actions.handleEmailCheck(lowerCaseMsg);
+    } else if (children.props.state.askingForGender) {
+      actions.handleGenderCheck(lowerCaseMsg);
+    } else if (children.props.state.askingForDistrict) {
       actions.handleDistrictCheck(lowerCaseMsg);
     } else if (children.props.state.askingForAge) {
       actions.handleAgeCheck(message);
-     }
-    //else if(children.props.state.showingOptions){
-    //         actions.handleSchemeSelection(message)
-    // }
+    } else if (children.props.state.askingForArea) {
+      actions.handleCheckArea(message);
+    } else if (children.props.state.askingForCategory) {
+      actions.handleCategoryCheck(message);
+    } else if (children.props.state.askingForDifferent) {
+      actions.handleDifferntialCheck(message);
+    } else if (children.props.state.askingForDifferentPercentage) {
+      actions.handleDiffPercentage(message);
+    } else if (children.props.state.askingForMinority) {
+      actions.handleDifferntialCheck(message);
+    } else if (children.props.state.askingForStudent) {
+      actions.handleStudentCheck(message);
+    } else if (children.props.state.askingForBPL) {
+      actions.handleBPLCheck(message);
+    } else if (children.props.state.askingForPenury) {
+      actions.handlePenuryCheck(message);
+    } else if (children.props.state.askingForAnnualIncome) {
+      actions.handleAnnualIncome(message);
+    } else if (children.props.state.askingForFinalResult) {
+      actions.handleFinalResult(message);
+    }
   };
 
   return (
