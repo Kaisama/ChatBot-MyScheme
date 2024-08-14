8577ea1e04c040978cf509a44554d4be3e8cc22e
Akshat Bisht <bishtakshat64@gmail.com>
2024-08-14 11:19:45 +0530

    config done

--- src/chatBot/cofig.jsx
+++ src/chatBot/cofig.jsx
@@ -3,18 +3,40 @@ import Avatar from './Avatar';
 import UserAvatar from './UserAvatar';
 import OptionsButton from './Options';
 import YesNo from './YesNo';
+import GenderOptions from './GenderOptions';
+import AreaOptions from './AreaOptions';
+import CategoryOptions from './CategoryOptions';
+import DifferentlyButton from './Differently';
+import StudentButton from './StudentButton';
+import BPLButtons from './BPLButtons';
+import Penury from './Penury';
+import MinorityButtons from './MinorityButtons';
 
 const config = {
   botName: "MyScheme Bot",
-  initialMessages: [createChatBotMessage(`In which district do you live in Uttarakhand?`)],
+  initialMessages :[createChatBotMessage('Enter Your Full Name')],
   customComponents: {
     botAvatar: (props) => <Avatar {...props} />,
     userAvatar: (props) => <UserAvatar {...props} />,
   },
   state: {
-    askingForDistrict: true,
+    askingForName:true,
+    askingForEmail:false,
+    askingForGender:false,
+    askingForDistrict: false,
     askingForAge: false,
     showingOptions: false,
+    askingForArea:false,
+    askingForCategory:false,
+    askingForDifferent:false,
+    askingForNoDifferent:false,
+    askingForDifferentPercentage:false,
+    askingForStudent:false,
+    askingForMinority:false,
+    askingForBPL:false,
+    askingForPenury:false,
+    askingForFinalResult:false,
+    askingForAnnualIncome:false
   },
   widgets: [
   {
@@ -24,6 +46,34 @@ const config = {
   {
     widgetName:'yesNo',
     widgetFunc:(props)=><YesNo {...props}/>
+  },   {
+    widgetName: 'GenderOptions',
+    widgetFunc: (props) => <GenderOptions {...props} />,
+  },{
+    widgetName:"AreaOptions",
+    widgetFunc:(props)=><AreaOptions {...props}/>
+  },
+  {
+    widgetName:"CategoryOptions",
+    widgetFunc:(props)=><CategoryOptions {...props}/>
+  },
+  {
+    widgetName:"DifferentlyButton",
+    widgetFunc:(props)=><DifferentlyButton {...props}/>
+
+  },
+  {
+    widgetName:'StudentButton',
+    widgetFunc:(props)=><StudentButton {...props}/>
+  },{
+    widgetName:'BPLButtons',
+    widgetFunc:(props)=><BPLButtons {...props}/>
+  },{
+    widgetName:'Penury',
+    widgetFunc:(props)=><Penury {...props}/>
+  },{
+    widgetName:"MinorityButtons",
+    widgetFunc:(props)=><MinorityButtons{...props}/>
   }
   ],
 };
